7eb0f54213fe0022c75112f3cebe79d0cf6a03b7
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2024-08-09 13:06:45 -0400

    Adds order test

---
+++ order_test.go
@@ -0,0 +1,22 @@
+package techan
+
+import (
+	"testing"
+	"time"
+
+	"github.com/sdcoffey/big"
+)
+
+func TestOrderCostBasis(t *testing.T) {
+	order := Order{
+		Side:          BUY,
+		Security:      "FAKE",
+		Price:         big.NewDecimal(10.00),
+		Amount:        big.NewDecimal(10.00),
+		ExecutionTime: time.Now(),
+	}
+
+	costBasis := order.CostBasis()
+
+	decimalEquals(t, 100.0, costBasis)
+}
