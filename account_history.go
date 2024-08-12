29f0ef2f25c47596eda7804033d60618b4286b6d
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2024-08-12 13:37:49 -0400

    adds pricing and securities metadata to account history

--- account_history.go
+++ account_history.go
@@ -1,7 +1,12 @@
 package techan
 
-import "github.com/sdcoffey/big"
+import (
+	"fmt"
 
+	"github.com/sdcoffey/big"
+)
+
+// An AccountSnapshot provides the point in time state of an account and its positions.
 type AccountSnapshot struct {
 	Period    TimePeriod
 	Equity    big.Decimal
@@ -9,6 +14,35 @@ type AccountSnapshot struct {
 	Positions []*PositionSnapshot
 }
 
+// The PricingSnapshot provides point in time pricing information for all tracked securities,
+// including the bechmark if used. The period for each snapshot should match a period supplied
+// in an account snapshot.
+type PricingSnapshot struct {
+	Period TimePeriod
+	Prices map[string]big.Decimal
+}
+
+// The AccountHistory contains a record of point in time account snapshots as well as a list of all
+// of the Securities tracked by the account over time.
 type AccountHistory struct {
-	snapshots []*AccountSnapshot
+	Benchmark  string
+	Securities []string
+	Prices     []*PricingSnapshot
+	Snapshots  []*AccountSnapshot
+}
+
+// Add a new account and pricing snapshot to the history.
+func (ah *AccountHistory) ApplySnapshot(accountSnapshot *AccountSnapshot, pricingSnapshot *PricingSnapshot) error {
+	if !accountSnapshot.Period.Start.Equal(pricingSnapshot.Period.Start) {
+		return fmt.Errorf(
+			"start periods for account and pricing snapshots do not match: %v <-> %v",
+			accountSnapshot.Period.Start,
+			pricingSnapshot.Period.Start,
+		)
+	}
+
+	ah.Snapshots = append(ah.Snapshots, accountSnapshot)
+	ah.Prices = append(ah.Prices, pricingSnapshot)
+
+	return nil
 }
