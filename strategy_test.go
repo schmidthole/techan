0f512eeaa41f441070c21a015708a0b0b4e51aff
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2024-08-13 16:29:07 -0400

    Adds simple strat test for the last index function

--- strategy_test.go
+++ strategy_test.go
@@ -1,1 +1,15 @@
 package techan
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStrategyLastIndex(t *testing.T) {
+	strat := Strategy{
+		Timeseries: *mockedTimeSeries,
+	}
+
+	assert.Equal(t, 11, strat.LastIndex())
+}
