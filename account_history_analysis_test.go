fda0a30964f26e18d2f426680a71d4fda4f18aa7
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2024-08-15 10:29:30 -0400

    add edge case tests to ah analysis

--- account_history_analysis_test.go
+++ account_history_analysis_test.go
@@ -33,6 +33,23 @@ func TestAccountHistory_TotalProfit(t *testing.T) {
 	decimalEquals(t, -1.0, profit)
 }
 
+func TestAccountHistory_PercentGainZeroStart(t *testing.T) {
+	period := NewTimePeriod(time.Now(), time.Hour*24)
+	acctSnap := AccountSnapshot{Period: period, Equity: big.NewDecimal(0.0)}
+	pricingSnap := PricingSnapshot{Period: period}
+
+	period2 := NewTimePeriod(time.Now().Add(time.Hour*24), time.Hour*24)
+	acctSnap2 := AccountSnapshot{Period: period2, Equity: big.NewDecimal(2.0)}
+	pricingSnap2 := PricingSnapshot{Period: period2}
+
+	ah := NewAccountHistory()
+	ah.ApplySnapshot(&acctSnap, &pricingSnap)
+	ah.ApplySnapshot(&acctSnap2, &pricingSnap2)
+
+	pg := ah.PercentGain()
+	decimalEquals(t, 0.0, pg)
+}
+
 func TestAccountHistory_PercentGain(t *testing.T) {
 	period := NewTimePeriod(time.Now(), time.Hour*24)
 	acctSnap := AccountSnapshot{Period: period, Equity: big.NewDecimal(1.0)}
@@ -57,6 +74,44 @@ func TestAccountHistory_PercentGain(t *testing.T) {
 
 	pg = ah.PercentGain()
 	decimalEquals(t, -100.0, pg)
+}
+
+func TestAccountHistory_BenchmarkBuyHoldTotalProfitStartDNE(t *testing.T) {
+	period := NewTimePeriod(time.Now(), time.Hour*24)
+	acctSnap := AccountSnapshot{Period: period, Equity: big.NewDecimal(1.0)}
+	pricingSnap := PricingSnapshot{Period: period, Prices: map[string]big.Decimal{}}
+
+	period2 := NewTimePeriod(time.Now().Add(time.Hour*24), time.Hour*24)
+	acctSnap2 := AccountSnapshot{Period: period2}
+	pricingSnap2 := PricingSnapshot{Period: period2, Prices: map[string]big.Decimal{"BENCH": big.NewDecimal(2.0)}}
+
+	ah := NewAccountHistory()
+	ah.Benchmark = "BENCH"
+
+	ah.ApplySnapshot(&acctSnap, &pricingSnap)
+	ah.ApplySnapshot(&acctSnap2, &pricingSnap2)
+
+	pg := ah.BenchmarkBuyHoldTotalProfit()
+	decimalEquals(t, 0.0, pg)
+}
+
+func TestAccountHistory_BenchmarkBuyHoldTotalProfitEndDNE(t *testing.T) {
+	period := NewTimePeriod(time.Now(), time.Hour*24)
+	acctSnap := AccountSnapshot{Period: period, Equity: big.NewDecimal(1.0)}
+	pricingSnap := PricingSnapshot{Period: period, Prices: map[string]big.Decimal{"BENCH": big.NewDecimal(2.0)}}
+
+	period2 := NewTimePeriod(time.Now().Add(time.Hour*24), time.Hour*24)
+	acctSnap2 := AccountSnapshot{Period: period2}
+	pricingSnap2 := PricingSnapshot{Period: period2, Prices: map[string]big.Decimal{}}
+
+	ah := NewAccountHistory()
+	ah.Benchmark = "BENCH"
+
+	ah.ApplySnapshot(&acctSnap, &pricingSnap)
+	ah.ApplySnapshot(&acctSnap2, &pricingSnap2)
+
+	pg := ah.BenchmarkBuyHoldTotalProfit()
+	decimalEquals(t, 0.0, pg)
 }
 
 func TestAccountHistory_BenchmarkBuyHoldTotalProfit(t *testing.T) {
@@ -85,6 +140,44 @@ func TestAccountHistory_BenchmarkBuyHoldTotalProfit(t *testing.T) {
 
 	pg = ah.BenchmarkBuyHoldTotalProfit()
 	decimalEquals(t, -1.0, pg)
+}
+
+func TestAccountHistory_BenchmarkBuyHoldPercentGainStartDNE(t *testing.T) {
+	period := NewTimePeriod(time.Now(), time.Hour*24)
+	acctSnap := AccountSnapshot{Period: period, Equity: big.NewDecimal(1.0)}
+	pricingSnap := PricingSnapshot{Period: period, Prices: map[string]big.Decimal{}}
+
+	period2 := NewTimePeriod(time.Now().Add(time.Hour*24), time.Hour*24)
+	acctSnap2 := AccountSnapshot{Period: period2}
+	pricingSnap2 := PricingSnapshot{Period: period2, Prices: map[string]big.Decimal{"BENCH": big.NewDecimal(2.0)}}
+
+	ah := NewAccountHistory()
+	ah.Benchmark = "BENCH"
+
+	ah.ApplySnapshot(&acctSnap, &pricingSnap)
+	ah.ApplySnapshot(&acctSnap2, &pricingSnap2)
+
+	pg := ah.BenchmarkBuyHoldPercentGain()
+	decimalEquals(t, 0.0, pg)
+}
+
+func TestAccountHistory_BenchmarkBuyHoldPercentGainEndDNE(t *testing.T) {
+	period := NewTimePeriod(time.Now(), time.Hour*24)
+	acctSnap := AccountSnapshot{Period: period, Equity: big.NewDecimal(1.0)}
+	pricingSnap := PricingSnapshot{Period: period, Prices: map[string]big.Decimal{"BENCH": big.NewDecimal(2.0)}}
+
+	period2 := NewTimePeriod(time.Now().Add(time.Hour*24), time.Hour*24)
+	acctSnap2 := AccountSnapshot{Period: period2}
+	pricingSnap2 := PricingSnapshot{Period: period2, Prices: map[string]big.Decimal{}}
+
+	ah := NewAccountHistory()
+	ah.Benchmark = "BENCH"
+
+	ah.ApplySnapshot(&acctSnap, &pricingSnap)
+	ah.ApplySnapshot(&acctSnap2, &pricingSnap2)
+
+	pg := ah.BenchmarkBuyHoldPercentGain()
+	decimalEquals(t, 0.0, pg)
 }
 
 func TestAccountHistory_BenchmarkBuyHoldPercentGain(t *testing.T) {
