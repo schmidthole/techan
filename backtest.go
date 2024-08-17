31e1200040bb64beeac3dd7c45cbf9b3f4b8cff4
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2024-08-16 23:27:38 -0400

    finish initial backtest module

--- backtest.go
+++ backtest.go
@@ -2,63 +2,83 @@ package techan
 
 // The Backtest is a holder struct to run a simulated trading strategy against an account.
 type Backtest struct {
-	Tick       int
-	Strategies []Strategy
-	Allocator  Allocator
-	Account    *Account
-	History    *AccountHistory
+	tick       int
+	strategies []Strategy
+	allocator  Allocator
+	account    *Account
+	history    *AccountHistory
 }
 
 // Create a new backtest with the provided strategies, allocator, and starting account.
 // All strategies are assumed to be normalized and have the same length, indexes, and periods.
 func NewBacktest(strategies []Strategy, allocator Allocator, account *Account) *Backtest {
 	backtest := Backtest{
-		Tick:       0,
-		Strategies: strategies,
-		Allocator:  allocator,
-		Account:    account,
-		History:    NewAccountHistory(),
+		tick:       0,
+		strategies: strategies,
+		allocator:  allocator,
+		account:    account,
+		history:    NewAccountHistory(),
 	}
 
 	// setup the initial account state to be one period before all data.
 	// this will serve as a starting point before orders are executed.
 	initialPeriod := strategies[0].Timeseries.Candles[0].Period.Advance(-1)
-	backtest.History.ApplySnapshot(
-		backtest.Account.ExportSnapshot(initialPeriod),
+	backtest.history.ApplySnapshot(
+		backtest.account.ExportSnapshot(initialPeriod),
 		&PricingSnapshot{Period: initialPeriod, Prices: Pricing{}},
 	)
 
 	return &backtest
 }
 
-// Execute the strategy for the given tick.
-func (b *Backtest) ExecuteTick() error {
-	prices := Pricing{}
-	for _, strat := range b.Strategies {
-		prices[strat.Security] = strat.Timeseries.Candles[b.Tick].ClosePrice
+// Run the backtest from start to finish.
+func (b *Backtest) Run() (*AccountHistory, error) {
+	for b.tick < b.lastTick() {
+		err := b.executeTick()
+		if err != nil {
+			return nil, err
+		}
+
+		b.advanceTick()
 	}
 
-	allocations := b.Allocator.Allocate(b.Tick, b.Strategies)
+	return b.history, nil
+}
 
-	tradePlan, err := CreateTradePlan(allocations, prices, b.Account)
+func (b *Backtest) executeTick() error {
+	prices := Pricing{}
+	for _, strat := range b.strategies {
+		prices[strat.Security] = strat.Timeseries.Candles[b.tick].ClosePrice
+	}
+
+	allocations := b.allocator.Allocate(b.tick, b.strategies)
+
+	tradePlan, err := CreateTradePlan(allocations, prices, b.account)
 	if err != nil {
 		return err
 	}
 
 	for _, order := range *tradePlan {
-		b.Account.ExecuteOrder(&order)
+		b.account.ExecuteOrder(&order)
 	}
 
-	period := b.Strategies[0].Timeseries.Candles[b.Tick].Period
-	b.History.ApplySnapshot(
-		b.Account.ExportSnapshot(period),
+	period := b.strategies[0].Timeseries.Candles[b.tick].Period
+	b.history.ApplySnapshot(
+		b.account.ExportSnapshot(period),
 		&PricingSnapshot{Period: period, Prices: prices},
 	)
 
 	return nil
 }
 
-// Advance the backtest's index tick.
-func (b *Backtest) AdvanceTick() {
-	b.Tick = b.Tick + 1
+func (b *Backtest) advanceTick() {
+	if b.tick >= b.strategies[0].Timeseries.LastIndex() {
+		return
+	}
+
+	b.tick = b.tick + 1
+}
+
+func (b *Backtest) lastTick() int {
+	return b.strategies[0].Timeseries.LastIndex()
 }
