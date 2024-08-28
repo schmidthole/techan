3f0352434a8d2fc6f562294a6d8c02ee1c27ff5b
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2024-08-28 14:42:51 -0400

    adds windowed percent change test

--- indicator_windowed_percent_change.go
+++ indicator_windowed_percent_change.go
@@ -19,16 +19,27 @@ func NewWindowedPercentChangeIndicator(indicator Indicator, window int) Indicato
 }
 
 func (s windowedPercentChangeIndicator) Calculate(index int) big.Decimal {
-	if index < s.window {
+	if s.window < 2 {
 		return big.ZERO
+	}
+
+	calcWindow := s.window - 1
+	if index < calcWindow {
+		calcWindow = index
 	}
 
 	end := s.indicator.Calculate(index)
-	start := s.indicator.Calculate(index - s.window)
+	start := s.indicator.Calculate(index - calcWindow)
 
 	if start.EQ(big.ZERO) {
+		if end.GT(big.ZERO) {
+			return big.NewDecimal(100.00)
+		} else if end.LT(big.ZERO) {
+			return big.NewDecimal(-100.00)
+		}
+
 		return big.ZERO
 	}
 
-	return end.Sub(start).Div(start)
+	return end.Sub(start).Div(start).Mul(big.NewFromInt(100))
 }
---
+++ indicator_windowed_percent_change_test.go
@@ -0,0 +1,26 @@
+package techan
+
+import (
+	"testing"
+
+	"github.com/sdcoffey/big"
+)
+
+func TestWindowedPercentChange(t *testing.T) {
+	ind := NewFixedIndicator(
+		1.0, 1.5, 2.0, 1.5, 1.0, 1.5, 2.0, 1.5, 1.0,
+	)
+	expected := []float64{
+		0.0, 50.0, 100.00, 0.0, -50.0, 0.0, 100.00, 0.0, -50.00,
+	}
+
+	pct := NewWindowedPercentChangeIndicator(ind, 3)
+
+	for i, want := range expected {
+		got := pct.Calculate(i)
+
+		if !got.EQ(big.NewDecimal(want)) {
+			t.Errorf("error in window pct change index %v. want: %v, got %v", i, want, got.String())
+		}
+	}
+}
