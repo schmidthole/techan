dbf6b799bd29f02a84ad6eaa71e0b93ccf599c9e
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2024-10-13 08:50:32 -0400

    add order types and tif fields

--- order.go
+++ order.go
@@ -7,12 +7,34 @@ import (
 )
 
 // OrderSide is a simple enumeration representing the side of an Order (buy or sell)
-type OrderSide int
+type OrderSide string
 
 // BUY and SELL enumerations
 const (
-	BUY OrderSide = iota
-	SELL
+	BUY  OrderSide = "BUY"
+	SELL OrderSide = "SELL"
+)
+
+// OrderType defines common order types accepted by brokers
+type OrderType string
+
+// OrderType enumerations, we only support the basics at this time
+// there are more complex types such as "stop limit" "trail" etc. that may be added later
+const (
+	MARKET OrderType = "MKT"
+	LIMIT  OrderType = "LMT"
+	STOP   OrderType = "STP"
+)
+
+// TimeInForce defines how long an order is good for before it is auto cancelled
+type TimeInForce string
+
+// TimeInForce option enumerations
+const (
+	GTC TimeInForce = "GTC"
+	OPG TimeInForce = "OPG"
+	DAY TimeInForce = "DAY"
+	IOC TimeInForce = "IOC"
 )
 
 // Order represents a trade execution (buy or sell) with associated metadata.
@@ -20,7 +42,9 @@ type Order struct {
 	Side          OrderSide
 	Security      string
 	Price         big.Decimal
+	Type          string
 	Amount        big.Decimal
+	TimeInForce   TimeInForce
 	ExecutionTime time.Time
 }
 
